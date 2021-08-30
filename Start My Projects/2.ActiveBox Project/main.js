395f737d345cfe3e46970e96b14aec745be6b61d
pologenki <pologenki@gmail.com>
2021-08-30 20:54:41 +0300

    members__item done

--- Start My Projects/2.ActiveBox Project/main.js
+++ Start My Projects/2.ActiveBox Project/main.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
         $('body').toggleclass('lock');
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    function hasTouchDevice() {
+      return !!('ontouchstart' in window || navigator.maxTouchPoints);
+    }
+    if (!hasTouchDevice()) {
+      const toUp = document.querySelector('.to-up');
+      toUp.onmouseenter = function(){
+        this.classList.add('hover');
+      }
+      toUp.onmouseleave = function() {
+        this.classList.remove('hover');
+      }
+    }
+  });

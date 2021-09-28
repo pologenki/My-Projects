f7202826e8b2b464c0543673282163202298a6f2
Pologenki <pologenki@gmail.com>
2021-09-28 18:37:39 +0300

    new uppdate

--- Start My Projects/Relvise Project/main.js
+++ Start My Projects/Relvise Project/main.js
@@ -4,3 +4,31 @@ $(document).ready(function() {
         $('body').toggleclass('lock');
     });
 });
+
+
+window.onload = function(){
+  document.body.scrollTop = 0;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    function hasTouchDevice() {
+      return !!('ontouchstart' in window || navigator.maxTouchPoints);
+    }
+    if (!hasTouchDevice()) {
+      const toUp = document.querySelector('.to-up');
+      toUp.onmouseenter = function(){
+        this.classList.add('hover');
+      }
+      toUp.onmouseleave = function() {
+        this.classList.remove('hover');
+      }
+    }
+  });
+
+  if (window.location.hash) {
+    setTimeout(function(){
+      window.scrollTo(0, 0);
+    }, 1);
+  }
+
+

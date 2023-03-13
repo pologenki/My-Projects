418b14f73eadd337f724380d685d538219ab74a2
pologenki <pologenki@gmail.com>
2023-03-13 17:33:54 +0200

    new gulp 2023

---
+++ Fls-Gulp 2023/src/js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunctions from "./modules/functions.js";
+import * as headerMenu from "./modules/headerMenu.js";
+import * as scrollPage from "./modules/scroll.js";
+
+//scrollPage.isWebp();
+headerMenu.isWebp();
+flsFunctions.isWebp();
+
+
+//Тут мы подключили некий свайпер Swiperjs.com
+import Swiper, { Navigation, Pagination } from 'swiper';
+/const swiper = new Swiper(...);/
